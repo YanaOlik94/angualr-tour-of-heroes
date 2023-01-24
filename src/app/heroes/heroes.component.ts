094a639b4ee0b86012510b4e06f96f2c5f649b91
yanaolik <yanaolik94@gmail.com>
2023-01-24 18:19:23 +0200

    Display a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import {Hero} from "../interface/hero";
+import {HEROES} from "../mock-heroes";
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.css']
 })
 
 export class HeroesComponent {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero?: Hero;
 
+  heroes = HEROES;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
